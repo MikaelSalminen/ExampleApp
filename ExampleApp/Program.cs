1ae07a6fe2b586c9faf94dc7befe154b189010e0
Mikael Salminen <mikael.salminen@koudata.fi>
2022-09-14 10:55:46 +0300

    Application initialized

---
+++ ExampleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, what is your name?");
+            string fname = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Your name is " + fname);
+            Console.ReadLine();
+        }
+    }
+}
